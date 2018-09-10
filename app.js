1b1059a010e9d22f9b4bad97b9cf7691d2e9df4b
serfer89 <sergpavlov89@gmail.com>
2018-09-11 00:21:47 +0300

    add mongod

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const bodyParser = require('body-parser');
 const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
 
-const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://localhost:27017/shc';
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'myproject';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+ 
+  const db = client.db(dbName);
+ 
+  insertDocuments(db, function() {
+    findDocuments(db, function() {
+      client.close();
+    });
+  });
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/api', product);
@@ -18,5 +34,3 @@ let port = 1234;
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
-
-
