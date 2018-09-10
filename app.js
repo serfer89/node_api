a993eb1be3a340f4b564f8dc0fe5c771032045ed
serfer89 <sergpavlov89@gmail.com>
2018-09-11 01:11:11 +0300

    add find

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ const url = 'mongodb://localhost:27017';
  
 // Database Name
 const dbName = 'shc';
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
  
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
@@ -21,12 +33,11 @@ MongoClient.connect(url, function(err, client) {
  
   const db = client.db(dbName);
  
-  insertDocuments(db, function() {
+  //insertDocuments(db, function() {
     findDocuments(db, function() {
       client.close();
     });
   });
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/api', product);
